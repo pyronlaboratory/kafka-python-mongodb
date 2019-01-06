bf0a49de57a99f6f9d837bd5004fe8cbbdcc9a6b
Ronyx <ronnie94official@gmail.com>
2019-01-07 02:05:37 +0530

    initial-commit

---
+++ lord-overload/kafka-python-mongodb.py
@@ -0,0 +1,13 @@
+from kafka import KafkaConsumer
+from pymongo import MongoClient
+from json import loads
+
+consumer = KafkaConsumer('lord-overload',bootstrap_servers=['localhost:9092'],auto_offset_reset='earliest',enable_auto_commit=True,group_id='my-group',value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+client = MongoClient('localhost:27017')
+collection = client.lord-overload.lord-overload
+
+for message in consumer:
+    message = message.value
+    collection.insert_one(message)
+    #print('{} added to {}'.format(message, collection))
